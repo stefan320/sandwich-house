2245f8b040e26d332a7917e813da5cba21e5ad9d
stefan320 <stefan_014_@hotmail.com>
2018-09-24 17:16:03 +0200

    image filter  distance update

--- js/main.js
+++ js/main.js
@@ -4,20 +4,18 @@
 // const navLogo = document.getElementById('nav-logo');
 // const mobUl = document.getElementById('mob-ul');  //The mobile nav ul id
 // const desktopNav = document.getElementById('desktop-nav');
-const startPage = document.getElementById('start-page');  //Traverse to the  content of main page
-const baguette = document.getElementById('baguette');
+const startPage = document.getElementById("start-page"); //Traverse to the  content of main page
+const baguette = document.getElementById("baguette");
 // const logoCenter = document.getElementById('logo-center');  //landing page center logo
-const animationText = document.getElementById('animation-text');
-const fresh = document.getElementById('fresh');
-const croissants = document.getElementById('croissants');
-const containerB = document.getElementById('container-b');
+const animationText = document.getElementById("animation-text");
+const fresh = document.getElementById("fresh");
+const croissants = document.getElementById("croissants");
+const containerB = document.getElementById("container-b");
 
 // When landing page is scrolled down event listener is fired
 // The event listener is immediatly removed
 // The centered logo is removed
 // the nav icon is shown while the animation for baguette croissant and animation text starts
-
-
 
 // function mediaQuery(x) {
 //     if (x.matches) {
@@ -27,39 +25,33 @@ const containerB = document.getElementById('container-b');
 //     }
 // }
 
-
 // const x = window.matchMedia("(min-width: 1000px)")
 
-
-
-function scrollToTop(){
-    window.scrollTo(0, 0);
+function scrollToTop() {
+  window.scrollTo(0, 0);
 }
 
-function startAnimation(){
+function startAnimation() {
   fresh.style.color = "#EAB766";
   baguette.style.right = "0";
   croissants.style.left = "0";
   animationText.style.left = "0";
 }
 
-function start(){
-    startPage.style.position = "relative";
-    scrollToTop();
-    containerB.style.paddingTop = "0";
-    // navLogo.style.display = "inline-block";
-    startAnimation();
-   mediaQuery(x) // Call listener function at run time
-   x.addListener(mediaQuery) // Attach listener function on state changes
+function start() {
+  startPage.style.position = "relative";
+  scrollToTop();
+  containerB.style.paddingTop = "0";
+  // navLogo.style.display = "inline-block";
+  startAnimation();
+  mediaQuery(x); // Call listener function at run time
+  x.addListener(mediaQuery); // Attach listener function on state changes
 }
 
+window.addEventListener("load", start);
 
-
-window.addEventListener("load",start);
-
-
-function checkDisplay(el){
-  return  window.getComputedStyle(el).display;
+function checkDisplay(el) {
+  return window.getComputedStyle(el).display;
 }
 //
 // function showNav(){
